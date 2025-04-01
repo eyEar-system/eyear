824fe321e70b1467d1cb76f798e56359cedf41d7
Ah777ed_said <ahmed01226313482@gmail.com>
2025-04-02 00:04:16 +0200

    Auto-sync update

---
+++ push.bat
@@ -0,0 +1,7 @@
+@echo off
+cd /d G:\.shortcut-targets-by-id\1bdlx6a5EJ-tobIHSQLkDecLuIURyniD9\_eyear_\git\eyear
+git add .
+git commit -m "Auto-sync update"
+git push origin main
+echo "push done scessfully 🚀"
+pause
