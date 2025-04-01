663b355f42a2f6458b406a076e7d7bd067c0afaa
eyear <eyearassistant@gmail.com>
2025-04-01 22:00:59 +0200

    Update firebase.py

--- eyear/firebase.py
+++ eyear/firebase.py
@@ -2,6 +2,20 @@
 #!pip install pyrebase4
 
 
+import pyrebase
+import firebase_admin
+from firebase_admin import credentials, db
+from firebase_admin import credentials as firebase_credentials, storage
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+import json
+from datetime import timedelta
+import firebase_admin
+from firebase_admin import credentials as firebase_credentials, storage
+from google.auth.transport.requests import Request
+from google.oauth2.service_account import Credentials
+import json
+from datetime import timedelta
 
 class FirebaseStorageManager:
     def __init__(self, service_account_json, bucket_name):
