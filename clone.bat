de3a185bb5637b619a5c6e5ced45961bbe7e9d6c
Ah777edSaid <your-email@example.com>
2026-01-03 09:04:01 +0200

    Auto-sync update

---
+++ clone.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Navigate to the desired directory
+cd /d "J:\My Drive\_eyear_\Software-code/eyear"
+
+REM Clone the GitHub repository
+git clone https://github.com/eyEar-system/eyear.git
+
+REM Navigate into the cloned repository
+cd eyear
+
+REM Optional: Open the cloned repository in a default editor, e.g., Visual Studio Code
+REM code .
+
+REM Pause to see any potential errors or confirmation messages
+pause
--- push.bat
+++ push.bat
@@ -1,7 +1,7 @@
 @echo off
-cd /d G:\.shortcut-targets-by-id\1bdlx6a5EJ-tobIHSQLkDecLuIURyniD9\_eyear_\hugingface\eyear
+cd /d "J:\My Drive\_eyear_\Software-code\eyear"
 git add .
 git commit -m "Auto-sync update"
 git push origin main
-echo "push done scessfully"
+echo "push done sucessfully "
 pause
